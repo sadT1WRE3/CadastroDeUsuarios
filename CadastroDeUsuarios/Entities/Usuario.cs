0048e3d36ff6f7186605fb6e4c03a1a46bc78716
Jodyson João <jodysom159@gmail.com>
2024-12-09 13:37:16 -0300

    Criado com sucesso o aplicativo de Cadastro, recursos adicionados: Confirmação de senha

---
+++ CadastroDeUsuarios/Entities/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+
+namespace CadastroDeUsuarios.Entities
+{
+    class Usuario
+    {
+        public string Email{ get; private set; }
+        public string Name { get; private set; }
+        public string Password { get; protected set; }
+
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(string email, string name, string senha)
+        {
+            Email = email;
+            Name = name;
+            Password = senha;
+        }
+
+        public override string ToString()
+        {
+            return
+                   "Informações do usuario:" +
+                   "Email: " + Email + "\n" + 
+                   "Nome: " + Name + "\n" +
+                   "Senha: " + Password;
+
+        }
+    }
+}
---
+++ CadastroDeUsuarios/Program.cs
@@ -0,0 +1,42 @@
+﻿using CadastroDeUsuarios.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CadastroDeUsuarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao nosso sistema de login ");
+            Console.WriteLine("Entre com os seus dados a seguir");
+            Console.WriteLine("Digite seu Email:");
+            string email = Console.ReadLine();
+            Console.WriteLine("Digite seu Nome:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Digite sua senha:");
+            string senha = (Console.ReadLine());
+            int confirmar = 1;
+
+            while (confirmar == 1)
+            {
+            
+            Console.WriteLine("Confirme a sua Senha:");
+            string senha2 = (Console.ReadLine());
+            if (senha == senha2)
+            {
+                confirmar = 0;
+            }
+            else
+            {
+                Console.WriteLine("As senhas não conferem, por favor confirme novamente");
+            }
+            }
+            Usuario user1 = new Usuario(email, name, senha);
+
+            Console.WriteLine(user1);
+        }
+    }
+}
+    
